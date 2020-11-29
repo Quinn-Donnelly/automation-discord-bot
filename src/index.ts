345c53401aaec686a4b3fa30d45c515661acceb4
Quinn Donnelly <quinndonnelly22@gmail.com>
2020-11-29 16:12:18 -0500

    Updated with the backbone of the simple server

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import {Client, Message} from 'discord.js';
+// @ts-ignore
+import {prefix, envTokenKey} from '../config.json'
+
+
+declare global {
+  namespace NodeJS {
+    export interface ProcessEnv {
+      "DISCORD_TOKEN": string;
+    }
+  }
+}
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client();
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('message', message => {
+  if (message.content.charAt(0) === prefix) {
+    handleCommand(message);
+  }
+});
+
+const handleCommand = (message: Message) => {
+  const [command, argsList] = removePrefix(message.content).split(" ");
+
+  switch (command) {
+    case "help":
+      message.channel.send("Help has not been implemented");
+      break;
+    default:
+      message.channel.send("Sorry command not found");
+  }
+}
+
+const removePrefix = (commandMessage: String) => {
+  return commandMessage.substring(prefix.length, commandMessage.length - prefix.length);
+}
+
+client.login(DISCORD_TOKEN);
